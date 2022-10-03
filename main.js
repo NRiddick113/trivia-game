ae7efc604d74eef5378b97dae6b0bcaf97480470
NRiddick113 <NatashaRiddick@pursuit.org>
2022-10-03 13:42:06 -0400

    working on css

--- main.js
+++ main.js
@@ -1,43 +1,117 @@
+const main = document.querySelectorAll('main')
 const question = document.querySelector('#question')
 const answers = document.querySelector('#answers')
 const next = document.querySelector('#next')
  const scoreSpan = document.querySelector('.score')
-//  const form = document.querySelector('')
-// console.log(question,answers,next)
+ const input = document.querySelectorAll('input')
+ const select = document.querySelectorAll('select')
+ const form = document.querySelector('form')
+console.log(question)
 
 
 let updatedResults;
 let index = 0
 let score = 0
 
-const updateQuestions = () => {
-    question.innerHTML = updatedResults[index].question
 
-for (let i = 0; i < 4; i++) {
-
-    const div = document.createElement('div')
-    const randomNum = Math.floor(Math.random() * updatedResults[index].incorrect_answers.length)
-
-    div.innerHTML = updatedResults[index].incorrect_answers.splice(randomNum, 1)
-
-    answers.append(div)
-}
-}
-
-const getQuestions = async () => {
-   
-    const qs = await fetch(`https://opentdb.com/api.php?amount=10&category=10&type=multiple`)
-const qsJson = await qs.json()
-// console.log(qsJson)
-
-updatedResults = qsJson.results.map((q) => {
-q.incorrect_answers.push(q.correct_answer)
-return q
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    // console.log(main[0].style.display)
+    main[0].style.display = 'block'
+    getQuestions()
+    form.style.display = 'none'
 })
 
-console.log(updatedResults)
-updateQuestions()
+function getQuestions () {
+    if (input[0].checked && select[0].options[0].selected) {
+        fetch(`https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple`)
+        .then(res => res.json())
+        .then(res => { 
+            console.log(res) 
+            updatedResults = res.results.map((q) => {
+                q.incorrect_answers.push(q.correct_answer)
+                return q
+            })
+            updateQuestions()
+        })
+        .catch(err => console.log(err))
+    }else if (input[0].checked && select[0].options[1].selected) {
+        fetch(`https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple`)
+        .then(res => res.json())
+        .then(res => { console.log(res) 
+            updatedResults = res.results.map((q) => {
+                q.incorrect_answers.push(q.correct_answer)
+                return q
+            })
+            updateQuestions()
+        })
+        .catch(err => console.log(err))
+    }else if (input[0].checked && select[0].options[2].selected) {
+        fetch(`https://opentdb.com/api.php?amount=10&category=15&difficulty=hard&type=multiple`)
+        .then(res => res.json())
+        .then(res => { console.log(res) 
+            updatedResults = res.results.map((q) => {
+                q.incorrect_answers.push(q.correct_answer)
+                return q
+            })
+            updateQuestions()
+        })
+        .catch(err => console.log(err))
+    }
+    
+    if (input[1].checked && select[0].options[0].selected) {
+        fetch(`https://opentdb.com/api.php?amount=10&category=16&difficulty=easy&type=multiple`)
+        .then(res => res.json())
+        .then(res => { console.log(res) 
+            updatedResults = res.results.map((q) => {
+                q.incorrect_answers.push(q.correct_answer)
+                return q
+            })
+            updateQuestions()
+        })
+        .catch(err => console.log(err))
+    }else if (input[1].checked && select[0].options[1].selected) {
+        fetch(`https://opentdb.com/api.php?amount=10&category=16&difficulty=medium&type=multiple`)
+        .then(res => res.json())
+        .then(res => { console.log(res) 
+            updatedResults = res.results.map((q) => {
+                q.incorrect_answers.push(q.correct_answer)
+                return q
+            })
+            updateQuestions()
+        })
+        .catch(err => console.log(err))
+    }else if (input[1].checked && select[0].options[2].selected) {
+        fetch(`https://opentdb.com/api.php?amount=10&category=16&difficulty=hard&type=multiple`)
+        .then(res => res.json())
+        .then(res => { console.log(res) 
+            updatedResults = res.results.map((q) => {
+                q.incorrect_answers.push(q.correct_answer)
+                return q
+            })
+            updateQuestions()
+        })
+        .catch(err => console.log(err))
+    }
+    
+    // console.log(updatedResults)
+    
+    
+}
 
+const updateQuestions = () => {
+    question.innerHTML = updatedResults[index].question
+    
+    for (let i = 0; i < 4; i++) {
+        
+        const div = document.createElement('div')
+        const randomNum = Math.floor(Math.random() * updatedResults[index].incorrect_answers.length)
+        
+        div.innerHTML = updatedResults[index].incorrect_answers.splice(randomNum, 1)
+        
+        answers.append(div)
+    }
 }
 
 next.addEventListener('click', () => {
@@ -51,7 +125,7 @@ next.addEventListener('click', () => {
     answers.style.pointerEvents = 'auto'
 })
 
-getQuestions()
+// getQuestions()
 
 answers.addEventListener('click', (event) =>{
     if (event.target.innerHTML === updatedResults[index].correct_answer) {
